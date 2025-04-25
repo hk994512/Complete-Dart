9c379e6b1f1f6af28b6ae973a25e89c2a2d74fd7
ᴍ ʜᴀᴍᴢᴀ ᴋʜᴀɴ <157797338+hk994512@users.noreply.github.com>
2025-04-25 10:16:32 -0700

    Create basic string operations.dart

---
+++ basic string operations.dart
@@ -0,0 +1,14 @@
+void main(){
+  String dart = 'Flutter and Dart';
+  // Converting to Upper Case 
+  print(dart.toUpperCase());
+  
+  // Similarly we can also convert it to UpperCase 
+ String x = 'Hi Dart'.toUpperCase();
+  print(x);
+  
+  // Converting literals to lower case 
+  String toLower = 'HAMZA'.toLowerCase();
+  print(toLower);
+  
+}
