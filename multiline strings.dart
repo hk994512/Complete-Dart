de22fabe2ba68ab494e2c6955fc4320a680e9498
ᴍ ʜᴀᴍᴢᴀ ᴋʜᴀɴ <157797338+hk994512@users.noreply.github.com>
2025-04-25 10:06:16 -0700

    Create multiline strings.dart

---
+++ multiline strings.dart
@@ -0,0 +1,4 @@
+void main(){
+  String bless = 'Hi my name is M Hamza\n I am from Pakistan\n I have done BSCS from there \n Know I am working on Mobile applications development\n Providing services for Android and Ios.\n Thank U';
+  print(bless);
+}
