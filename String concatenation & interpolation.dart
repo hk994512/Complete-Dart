c5c1cbea113cec65cc19970885cfb0c95751de78
ᴍ ʜᴀᴍᴢᴀ ᴋʜᴀɴ <157797338+hk994512@users.noreply.github.com>
2025-04-25 09:58:07 -0700

    Create String concatenation & interpolation.dart

---
+++ String concatenation & interpolation.dart
@@ -0,0 +1,9 @@
+void main() {
+   double temp = 20.0;
+  int value = 3;
+  String pizza= "pizza";
+  String pasta = "pasta";
+  int sum = value+value;
+//   print('The temperature is ' + temp.toString() + value.toString() +'+' + value.toString()+'makes'+sum.toString()+'I Like '+pizza + 'and '+pasta);
+  print('The temperature is $temp\t ${value} + ${value} makes $sum\t I like $pizza and $pasta');
+}
